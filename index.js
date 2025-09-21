390a61c40d538e8b1e0d6deef4d8301c81cbed55
Mahmoud Nagdy <mahmoudnagdy101@gmail.com>
2025-09-21 14:23:19 +0300

    first

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import { bootstrap } from './src/index.router.js'
+import cors from "cors";
 dotenv.config()
 
 const app = express()
+
+app.use(cors({origin: '*'}));
 
 
 app.get('/', (req, res, next) => {
