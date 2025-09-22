51845ce89fd664844892860fbf6345bd32f05df9
Mahmoud Nagdy <mahmoudnagdy101@gmail.com>
2025-09-22 19:12:47 +0300

    Update auth.validationSchema.js

--- src/modules/auth/auth.validationSchema.js
+++ src/modules/auth/auth.validationSchema.js
@@ -5,7 +5,7 @@ import Joi from "joi";
 export const signupSchema = {
 
     body: Joi.object({
-        name: Joi.string().min(3).max(20),
+        name: Joi.string().min(6).max(20),
         email: Joi.string().email(),
         password: Joi.string().min(6),
         confirmPassword: Joi.string().valid(Joi.ref('password'))
